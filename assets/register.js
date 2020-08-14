11b277fd6394a608a721c47377224d3565d299ac
Ninh Tomosia <tt.ninh.le@tomosia.com>
2020-08-14 16:58:40 +0700

    first commit

---
+++ assets/register.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//     function getMsg(selector) {
+//         return $(selector).attr('data-msg');
+//     }
+    
+//     $('#form-signup').validate({
+//         // ...
+//         messages: {
+//             firstName: getMsg('#fullname'),
+//             lastName: getMsg('#email')
+//         }
+//         // ...
+//     });
+// });
